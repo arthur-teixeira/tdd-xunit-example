0bc0461d9904e7f4f88dc1d1ed5c2c04155b1b40
arthur-carvalho <arthur.teixeira@objective.com.br>
2022-11-28 15:54:28 -0300

    Dinamically calling test subject

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
-
+        self.name = name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
     def testMethod(self):
         self.wasRun = 1
     
-    def run(self):
-        self.testMethod()
 
 test= WasRun("testMethod")
 print(test.wasRun)
