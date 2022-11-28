987a39ab2b330b6ac4a8a2960c6c4cf000125652
arthur-carvalho <arthur.teixeira@objective.com.br>
2022-11-28 16:48:41 -0300

    remove setUp

--- main.py
+++ main.py
@@ -19,10 +19,8 @@ class WasRun(TestCase):
         self.log = self.log + "testMethod "
     
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
+        self.test = WasRun("testMethod")
         self.test.run()
         assert("setUp testMethod " == self.test.log)
 
