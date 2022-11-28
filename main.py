39266b0052c5a32fe20d352d093b8eb7351ffcc1
arthur-carvalho <arthur.teixeira@objective.com.br>
2022-11-28 16:39:10 -0300

    Test setup method

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        self.wasSetUp = 1
     def run(self):
+            self.setUp()
             method = getattr(self, self.name)
             method()
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
+
+    def setUp(self):
+        self.wasRun= None
+        self.wasSetUp= 1
+        
     def testMethod(self):
         self.wasRun = 1
     
 class TestCaseTest(TestCase):
     def testRunning(self):
         test= WasRun("testMethod")
-        assert(not test.wasRun)
         test.run()
         assert(test.wasRun)
+
+    def testSetup(self):
+        test= WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetup").run()
