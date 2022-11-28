d8c268882a88519a370336cd61d1ba60af58ce5d
arthur-carvalho <arthur.teixeira@objective.com.br>
2022-11-28 17:23:18 -0300

    Summary catches failing setUp

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ class TestCase:
     def run(self):
             result = TestResult()
             result.testStarted()
-            self.setUp()
-            method = getattr(self, self.name)
-            method()
+            try:
+                self.setUp()
+                method = getattr(self, self.name)
+                method()
+            except:
+                result.testFailed()
             self.tearDown()
             return result
 
@@ -19,32 +22,33 @@ class TestResult:
     def __init__(self) -> None:
         self.runCount = 0
         self.errorCount = 0
+
     def testStarted(self):
         self.runCount += 1
+
     def testFailed(self):
         self.errorCount +=1
+
     def summary(self):
         return "%d run, %d failed" % (self.runCount, self.errorCount)
 class WasRun(TestCase):
-    def __init__(self, name):
-        TestCase.__init__(self, name)
-
     def setUp(self):
         self.log = "setUp "
-        self.wasRun= None
+
     def tearDown(self):
         self.log += "tearDown "
+
     def testMethod(self):
         self.log += "testMethod "
+
     def testBrokenMethod(self):
         raise Exception
 
-    
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
-        self.test = WasRun("testMethod")
-        self.test.run()
-        assert("setUp testMethod tearDown " == self.test.log)
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
     
     def testResult(self):
         test = WasRun("testMethod")
@@ -62,8 +66,16 @@ class TestCaseTest(TestCase):
         result.testFailed()
         assert("1 run, 1 failed" == result.summary())
 
+class FailingSetUpTestCaseTest(TestCase):
+    def setUp(self):
+        raise Exception
+    def testFailOnSetUp(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
 TestCaseTest("testFailedResultFormatting").run()
-# TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResult").run()
+FailingSetUpTestCaseTest("testFailOnSetUp").run()
