6ce6c83bf28829d914a354721a26ac141db70c1e
arthur-carvalho <arthur.teixeira@objective.com.br>
2022-11-28 16:48:30 -0300

    Refactor to use logs

--- main.py
+++ main.py
@@ -12,24 +12,19 @@ class WasRun(TestCase):
         TestCase.__init__(self, name)
 
     def setUp(self):
+        self.log = "setUp "
         self.wasRun= None
-        self.wasSetUp= 1
         
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
     
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testRunning(self):
+    def testTemplateMethod(self):
         self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetup(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+        assert("setUp testMethod " == self.test.log)
 
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetup").run()
+TestCaseTest("testTemplateMethod").run()
